c3e41732663c8e75e3bb2067ba586658fa5d74bd
KimKyuHoi <k546kh@gmail.com>
2025-04-01 15:57:28 +0900

    build: 절대 경로 및 antd, emotion 라이브러리 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      types: path.resolve(__dirname, './src/types'),
+      utils: path.resolve(__dirname, './src/utils'),
+      pages: path.resolve(__dirname, './src/shared'),
+    },
+  },
+});
