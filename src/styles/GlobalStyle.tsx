50f5569c06c24c9cf1c5df9f3c72951cecfcff34
KimKyuHoi <k546kh@gmail.com>
2025-04-02 15:27:42 +0900

    feat: GlobalStyle 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -7,6 +7,11 @@ const GlobalStyle = () => (
         color: #4a7cfe !important;
         font-weight: 400 !important;
       }
+
+      .custom-dropdown .ant-select-item-option-active {
+        background-color: #0000000a !important;
+        color: #000000e0 !important;
+      }
     `}
   />
 );
