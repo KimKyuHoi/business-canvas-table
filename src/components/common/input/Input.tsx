cc932c4412e8d59b416dab48dfc8a8097642d89f
KimKyuHoi <k546kh@gmail.com>
2025-04-02 13:19:33 +0900

    fix: input css 수정

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,6 +1,7 @@
 import { Input as AntInput } from 'antd';
 import { ChangeEvent } from 'react';
 import type { InputProps } from 'antd';
+import { css } from '@emotion/react';
 
 type CustomInputProps = {
   placeholder: string;
@@ -19,6 +20,11 @@ const CustomInput = ({
       placeholder={placeholder}
       status={status}
       onChange={onChange}
+      css={css`
+        border-radius: 8px;
+        padding: 0px 12px;
+        height: 32px;
+      `}
       {...rest}
     />
   );
