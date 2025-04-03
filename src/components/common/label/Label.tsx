2091fae4f74f1abe3aca80f24c61a6be2ca689d4
KimKyuHoi <k546kh@gmail.com>
2025-04-03 19:49:00 +0900

    build: jest 환경 제거 및 테스트 코드 제거

--- src/components/common/label/Label.tsx
+++ src/components/common/label/Label.tsx
@@ -3,38 +3,34 @@ import { css } from '@emotion/react';
 type LabelProps = {
   label: string;
   required?: boolean;
+  htmlFor?: string;
 };
 
-const CustomLabel = ({ label, required = false }: LabelProps) => {
+const CustomLabel = ({ label, required = false, htmlFor }: LabelProps) => {
   return (
-    <div
+    <label
+      htmlFor={htmlFor}
       css={css`
         display: flex;
         align-items: center;
         height: 40px;
+        font-size: 16px;
+        font-weight: 600;
+        color: #00000073;
       `}
     >
-      <span
-        css={css`
-          font-size: 16px;
-          font-weight: 600;
-          color: #00000073;
-        `}
-      >
-        {label}
-        {required && (
-          <span
-            css={css`
-              color: #ff4d4f;
-              margin-left: 4px;
-              font-size: 16px;
-            `}
-          >
-            *
-          </span>
-        )}
-      </span>
-    </div>
+      {label}
+      {required && (
+        <span
+          css={css`
+            color: #ff4d4f;
+            margin-left: 4px;
+          `}
+        >
+          *
+        </span>
+      )}
+    </label>
   );
 };
 
